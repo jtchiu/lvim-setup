2e77e95f482f873e5b73e96eb4520d687fa79773
jtchiu <jtchiu8@gmail.com>
2023-08-14 22:50:36 -0400

    update config

--- config.lua
+++ config.lua
@@ -4,14 +4,17 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 vim.opt.cmdheight = 2             -- more space in the neovim command line for displaying messages
 vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
-vim.opt.shiftwidth = 2            -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2               -- insert 2 spaces for a tab
+vim.opt.shiftwidth = 4            -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4               -- insert 2 spaces for a tab
 vim.opt.relativenumber = true     -- relative line numbers
 vim.opt.wrap = true               -- wrap lines
 
 -- use treesitter folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+vim.opt.colorcolumn = "80"
+vim.lsp.buf.hover()
 
 require("user.options")
 require("user.plugins")
@@ -20,9 +23,9 @@ require("user.which_key")
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  {
-    command = "prettier",
-    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-    cmd = { "typescript-language-server", "--stdio" }
-  },
+    {
+        command = "prettier",
+        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+        cmd = { "typescript-language-server", "--stdio" }
+    },
 }
